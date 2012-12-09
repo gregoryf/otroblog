5156c6bb960588f552f6b20d1ade4a0aa279f1ec
gregoryf <gregorynfisher@gmail.com>
2012-12-09 15:36:03 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+==Otro Blog
+
+Another blog. Nothing special.
+
+Features:
+* posts
+* categories
+* comments (threaded)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Otroblog::Application.config.secret_token = '8710a8ca4d57181f81fb4fc2562a49a5afe9fa58a2fc53f3655062eaef91ce05e18607ccfa25b448edef64ba35437182e789d12a86a828011fe2dce942f27a35'
